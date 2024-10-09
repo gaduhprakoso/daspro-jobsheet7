c4254fa4536aed183bc2e7afcf27141b35539991
gaduhprakoso <gaduhprakoso@gmail.com>
2024-10-09 20:40:31 +0700

    commit kedua

--- SiakadFor10.java
+++ SiakadFor10.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Siakad10 {
+public class SiakadFor10 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, batas = 60;
+        int lulus = 0, gagal = 0;
     
-
     for (int i = 1; i <= 10; i++) {
         System.out.print("masukan nilai mahasiswa ke-"+ i +":");
         nilai = sc.nextDouble();
@@ -17,9 +17,15 @@ public class Siakad10 {
         if (nilai < terendah){
             terendah = nilai;
         }
+        if (nilai >= batas){
+            lulus++;
+        }else{
+            gagal++;
+        }
     }
-
     System.out.println("Nilai tertinggi: "+ tertinggi);
     System.out.println("Nilai terendah: "+ terendah);
+    System.out.println("Jumlah Mahasiswa Lulus: "+ lulus);
+    System.out.println("Jumlah mahasiswa Tidak Lulus: "+ gagal);
     }
 }
